45d7d3ac075e51b2776a4d0f7387b20e5fde28fd
johan <jobando0730@gmail.com>
2019-07-03 17:24:48 -0500

    Ava

--- runicp.py
+++ runicp.py
@@ -8,29 +8,31 @@ import subprocess
 if __name__ == '__main__':
 
 	#------- rutas hacia las nubes de puntos y el ejecutable----------
-	icp_path="/home/johan/repos/GitHub/3D-Reconstruction/"
-	pointcloud1_path="/home/johan/repos/GitHub/3D-Reconstruction/Dataset_csv/points1.pcd"
-	pointcloud2_path="/home/johan/repos/GitHub/3D-Reconstruction/Dataset_csv/points137.pcd"
-
-
+	# .cpp file: addres /home/johan/Libraries/libpointmatcher/examples
+	icp_path="/home/johan/Libraries/libpointmatcher/build/examples"
+	pointcloud1_path="/home/johan/repos/GitHub/3D-Reconstruction/Dataset_csv/pointscsv1.csv"
+	pointcloud2_path="/home/johan/repos/GitHub/3D-Reconstruction/Dataset_csv/pointscsv137.csv"
+	
 	#--------------- correr el ejecutable de c++ ------------------
-	runicp = subprocess.Popen([os.path.join(icp_path,"icp_simple"),pointcloud1_path,pointcloud2_path],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	#runicp = subprocess.Popen([os.path.join(icp_path,"icp_simple"),pointcloud1_path,pointcloud2_path],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	runicp = subprocess.Popen([os.path.join(icp_path,"icp_simple"),pointcloud1_path,pointcloud2_path,'HI'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	runicp.wait()
 
 	#-------------leer los datos de devuelve en el terminal------
 	stdout, stderr = runicp.communicate()
-	print('stdout:\n',stdout)
-	print('stderr:\n',stderr)
-
+	print('stdout:',stdout)
+	#print('stderr:',stderr)
 
 	#----------------Guadar los datos en txt---------------------
-	#file = open("logs.txt","a")
-	#file.write("-------------------------------------\n")
+	'''
+	file = open("logs.txt","a")
+	file.write("-------------------------------------\n")
 	#simplejson.dump(maxiooas, file)
-	#file.write(stdout)
-	#file.write("-------------------------------------\n")
-	#file.close()
-
+	file.write(str(stdout))
+	file.write("-------------------------------------\n")
+	file.close()
 	#print(np.char.split(stdout, sep = ' '))
+	'''
+
     	
 
